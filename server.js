9d08120c9ae4e1713aba0c757fd84133f3cf3cc1
GutsySpider <mbowman@byupathway.edu>
2026-03-05 21:41:13 -0800

    created project

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const static = require("./routes/static");
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.set("layout", "./laouts/layout");
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
